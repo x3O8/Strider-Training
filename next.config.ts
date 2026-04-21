3f436f1b1d08fdc0cdb89fac612da5ca510fbe9c
x3O8 <abhijithkrishna200@gmail.com>
2026-04-21 14:49:31 +0530

    fixed deployment error

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
 
-  // Required for GitHub Pages (repo name)
-  basePath: '/Strider-Training',
-  assetPrefix: '/Strider-Training/',
-
-  // Needed because Image Optimization doesn't work in static export
   images: {
-    unoptimized: true,
+    unoptimized: true, // optional (can keep or remove)
   },
 };
 
